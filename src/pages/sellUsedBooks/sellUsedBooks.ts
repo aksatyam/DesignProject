b7cd017ac75487ab84a0a5849737c3f83bcdfbd0
aksatyam <ashishsatyam.kmr@gmail.com>
2018-04-05 14:41:09 +0530

    sell your books page updated

--- src/pages/sellUsedBooks/sellUsedBooks.ts
+++ src/pages/sellUsedBooks/sellUsedBooks.ts
@@ -12,6 +12,7 @@ declare var cordova: any;
 })
 export class SellUsedBooksPage {
   lastImage: string = null;
+  firstImage: string =null;
   constructor(  public navCtrl: NavController,
                 public camera:Camera,
                 public actionSheetCtrl: ActionSheetController, 
@@ -21,21 +22,21 @@ export class SellUsedBooksPage {
 
   }
 
-
-  public presentActionSheet() {
+  //Front Page Book
+  public presentActionSheetFront() {
     let actionSheet = this.actionSheetCtrl.create({
       title: 'Select Image Source',
       buttons: [
         {
           text: 'Load from Library',
           handler: () => {
-            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+            this.takePictureFront(this.camera.PictureSourceType.PHOTOLIBRARY);
           }
         },
         {
           text: 'Use Camera',
           handler: () => {
-            this.takePicture(this.camera.PictureSourceType.CAMERA);
+            this.takePictureFront(this.camera.PictureSourceType.CAMERA);
           }
         },
         {
@@ -47,7 +48,35 @@ export class SellUsedBooksPage {
     actionSheet.present();
   }
 
-  public takePicture(sourceType) {
+//rare Page Book
+public presentActionSheetRare() {
+  let actionSheet = this.actionSheetCtrl.create({
+    title: 'Select Image Source',
+    buttons: [
+      {
+        text: 'Load from Library',
+        handler: () => {
+          this.takePictureRare(this.camera.PictureSourceType.PHOTOLIBRARY);
+        }
+      },
+      {
+        text: 'Use Camera',
+        handler: () => {
+          this.takePictureRare(this.camera.PictureSourceType.CAMERA);
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel'
+      }
+    ]
+  });
+  actionSheet.present();
+}
+
+
+//Front Book Page
+  public takePictureFront(sourceType) {
   // Create options for the Camera Dialog
     var options = {
                     quality: 100,
@@ -61,19 +90,46 @@ export class SellUsedBooksPage {
         this.filepath.resolveNativePath(imagepath).then(path=>{
           let correctPath = path.substr(0, path.lastIndexOf('/') + 1);
           let currentName = path.substring(imagepath.lastIndexOf('/') + 1, imagepath.lastIndexOf('?'));
-          this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+          this.copyFileToLocalDirFront(correctPath, currentName, this.createFileName());
         })
       }
       else{
         var currentName = imagepath.substr(imagepath.lastIndexOf('/') + 1);
       var correctPath = imagepath.substr(0, imagepath.lastIndexOf('/') + 1);
-      this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+      this.copyFileToLocalDirFront(correctPath, currentName, this.createFileName());
       }
     },(err)=>{
       this.presentToast('Error while selecting image.');
     });
   }
 
+  //Rare Book Page
+  public takePictureRare(sourceType) {
+    // Create options for the Camera Dialog
+      var options = {
+                      quality: 100,
+                      sourceType: sourceType,
+                      saveToPhotoAlbum: false,
+                      correctOrientation: true
+                    };
+  
+      this.camera.getPicture(options).then(imagepath=>{
+        if(this.platform.is('android') && sourceType===this.camera.PictureSourceType.PHOTOLIBRARY){
+          this.filepath.resolveNativePath(imagepath).then(path=>{
+            let correctPath = path.substr(0, path.lastIndexOf('/') + 1);
+            let currentName = path.substring(imagepath.lastIndexOf('/') + 1, imagepath.lastIndexOf('?'));
+            this.copyFileToLocalDirRare(correctPath, currentName, this.createFileName());
+          })
+        }
+        else{
+          var currentName = imagepath.substr(imagepath.lastIndexOf('/') + 1);
+        var correctPath = imagepath.substr(0, imagepath.lastIndexOf('/') + 1);
+        this.copyFileToLocalDirRare(correctPath, currentName, this.createFileName());
+        }
+      },(err)=>{
+        this.presentToast('Error while selecting image.');
+      });
+    }
 
   // Create a new name for the image
 private createFileName() {
@@ -83,14 +139,25 @@ private createFileName() {
   return newFileName;
 }
  
+//Front Book Page
 // Copy the image to a local folder
-private copyFileToLocalDir(namePath, currentName, newFileName) {
+private copyFileToLocalDirFront(namePath, currentName, newFileName) {
+  this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+    this.firstImage = newFileName;
+  }, error => {
+    this.presentToast('Error while storing file.');
+  });
+}
+
+//Rare Book Page
+private copyFileToLocalDirRare(namePath, currentName, newFileName) {
   this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
     this.lastImage = newFileName;
   }, error => {
     this.presentToast('Error while storing file.');
   });
 }
+
   private presentToast(text) {
     let toast = this.toastCtrl.create({
       message: text,
