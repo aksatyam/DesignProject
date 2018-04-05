246df9b194826707456444b639dbe1303944b3a4
aksatyam <ashishsatyam.kmr@gmail.com>
2018-04-05 15:26:26 +0530

    My Listed Books Page added

---
+++ src/pages/myListedBooks/myListedBooks.ts
@@ -0,0 +1,124 @@
+import { Component } from '@angular/core';
+import { NavController} from 'ionic-angular';
+
+@Component({
+  selector: 'page-myListedBooks',
+  templateUrl: 'myListedBooks.html'
+})
+
+export class MyListedBooksPage {
+  public title:any="First Semester";
+  public cardArray:any=[
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                        {
+                          'book_name':'OOP with C++',
+                          'quantity':'1Pc',
+                          'price':'325',
+                          'selling_price':'150',
+                          'status':'In Stock'
+                        },
+                      ];
+
+
+    constructor(public navCtrl: NavController) {
+      
+    }
+
+}
