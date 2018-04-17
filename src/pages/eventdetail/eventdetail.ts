04d4790c8e72c0257fe524cf4b68b50ac9d0c904
aksatyam <ashishsatyam.kmr@gmail.com>
2018-04-17 13:14:44 +0530

    updated pages by Vishal

--- src/pages/eventdetail/eventdetail.ts
+++ src/pages/eventdetail/eventdetail.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the EventdetailPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
 @Component({
   selector: 'page-eventdetail',
